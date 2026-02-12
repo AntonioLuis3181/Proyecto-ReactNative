7a09f358f58dfebc7b1112193007ef532b5e5b39
AntonioLuis3181 <xavevel449@ieshnosmachado.org>
2026-02-12 14:33:58 +0100

    Update

--- mobile/src/service/api.js
+++ mobile/src/service/api.js
@@ -1,10 +1,13 @@
 // mobile/src/api.js
 import axios from 'axios';
 
+// ⚠️ CAMBIO AQUÍ: De 8081 a 3000
+// Si tu backend dice "Server running on port 3000", pon 3000.
 const API_URL = 'http://192.168.1.173:3000/api'; 
 
 const api = axios.create({
     baseURL: API_URL,
+    timeout: 10000,
 });
 
 export default api;
